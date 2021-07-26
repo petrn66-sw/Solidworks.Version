9759da4853eeba69135597e46b6f8fab06cf5e10
PeNo <petr.novotny@solidvision.cz>
2021-07-26 14:11:42 +0200

    Add project files.

---
+++ Solidworks.Versioning/Version.cs
@@ -0,0 +1,18 @@
+﻿namespace Solidworks.Versioning
+{
+    public enum Version
+    {
+        SW2010 = 18,
+        SW2011 = 19,
+        SW2012 = 20,
+        SW2013 = 21,
+        SW2014 = 22,
+        SW2015 = 23,
+        SW2016 = 24,
+        SW2017 = 25,
+        SW2018 = 26,
+        SW2019 = 27,
+        SW2020 = 28,
+        SW2021 = 29,
+    }
+}
